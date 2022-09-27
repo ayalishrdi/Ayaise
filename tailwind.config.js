7a04c2f8488a5039535cac8a25e865c18cb001e9
ayalishrdi <chyali.suhardi@gmail.com>
2022-09-27 22:07:16 +0700

    belajar-tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    container:{
+      extend: {
+        colors:{
+          primary: '',
+        }
+      },
+    }
+
+  
+   
+  },
+  plugins: [],
+}
